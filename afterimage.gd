ef05434bb5ee945bc2d2e2ca0b8db951ea1ca06d
Russell <rregan915@gmail.com>
2026-04-12 19:56:21 -0500

    afterimage mplement

--- afterimage.gd
+++ afterimage.gd
@@ -1,16 +1,17 @@
 extends Bullet
 
+var dir  
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var tw = create_tween()
 	tw.set_ease(Tween.EASE_IN)
-	tw.tween_property(self,"position",Vector2(150,0), 1)
-	tw.tween_property(self,"position",Vector2(8000,0), 3)
+	tw.tween_property(self,"position",position + Vector2(dir*150,0), 1)
+	tw.tween_property(self,"position",position + Vector2(dir*8000,0), 3)
+	scale.x = dir
 	
-	
-	
-	pass # Replace with function body.
+func on_hit():
+	pass	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
